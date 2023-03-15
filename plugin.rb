4bcd76b546273d3d536eb364b431cf368f1754ab
Tryps <86132393+DMTryptamines@users.noreply.github.com>
2023-03-15 06:57:15 -0300

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -7,14 +7,34 @@
 # url: TODO
 # required_version: 2.7.0
 
+::BLOG_HOST = Rails.env.development? ? "onion.tryp.digital"
+::BLOG_DISCOURSE = Rails.env.development? ? "tryp226d7fpfih3tx6i2gfawp4fedzwipunedekpgigpcfiy3pav6wyd.onion" 
+
 enabled_site_setting :plugin_name_enabled
 
 module ::MyPluginModule
-  PLUGIN_NAME = "discourse-plugin-name"
+  PLUGIN_NAME = "dimitris-plugin"
 end
 
 require_relative "lib/my_plugin_module/engine"
 
 after_initialize do
   # Code which should run after Rails has finished booting
+    # got to patch this class to allow more hostnames
+  class ::Middleware::EnforceHostname
+    def call(env)
+      hostname = env[Rack::Request::HTTP_X_FORWARDED_HOST].presence || env[Rack::HTTP_HOST]
+
+      env[Rack::Request::HTTP_X_FORWARDED_HOST] = nil
+
+      if hostname == ::BLOG_HOST
+        env[Rack::HTTP_HOST] = ::BLOG_HOST
+      else
+        env[Rack::HTTP_HOST] = ::BLOG_DISCOURSE
+      end
+
+      @app.call(env)
+    end
+  end
+
 end
